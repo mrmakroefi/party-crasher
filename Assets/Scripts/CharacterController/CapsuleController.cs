4bd54fb9c2641254c7a6627877de9e442624c5aa
Fathony Teguh Irawan <fathony.teguh@gmail.com>
2016-12-05 11:38:47 +0700

    first commit
    Signed-off-by: Fathony Teguh Irawan <fathony.teguh@gmail.com>

---
+++ Assets/Scripts/CharacterController/CapsuleController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class CapsuleController : MonoBehaviour
+{
+
+    public float moveSpeed = 6f;
+    public float rotationSpeed = 80f;
+
+    private Rigidbody playerRigidbody;
+    private Vector3 movement;
+    private Vector3 rotation;
+
+    void Awake ()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+	void Start ()
+	{
+
+	}
+	
+	void Update ()
+	{
+        
+    }
+
+    void FixedUpdate ()
+    {
+        InputDevice controller = InputManager.ActiveDevice;
+
+        float leftHorizontal = controller.LeftStickX;
+        float leftVertical = controller.LeftStickY;
+
+        float rightHorizontal = controller.RightStickX;
+        float rightVertical = controller.RightStickY;
+
+        Move(leftHorizontal, leftVertical);
+        if(rightHorizontal != 0f && rightVertical != 0) Rotate(rightHorizontal, rightVertical);
+    }
+
+    void Move(float h, float v)
+    {
+        movement.Set(h, 0f, v);
+
+        movement = movement.normalized * moveSpeed * Time.deltaTime;
+
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    void Rotate(float h, float v)
+    {
+        rotation.Set(h, 0f, v);
+
+        Quaternion newRotation = Quaternion.LookRotation(rotation);
+        Quaternion currentRotation = Quaternion.Lerp(playerRigidbody.rotation, newRotation, rotationSpeed * Time.deltaTime);
+
+        playerRigidbody.MoveRotation(currentRotation);
+    }
+}
