88a24d40c2f27e71d78b41d515746bc58e3d7bd0
Muhammad Rizal Ma'rufi <mrm_int_main@yahoo.com>
2016-12-25 13:38:56 +0700

    Add files via upload
    weapon

---
+++ Health.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+    public int maxHitPoint = 30;
+
+    private int hitPoint;
+
+    void Start() {
+        Reset();
+    }
+
+    public void Damage(int damage) {
+        hitPoint -= damage;
+        if ( hitPoint <= 0 ) {
+            // dead
+            gameObject.SetActive(false);
+        }
+    }
+
+    void Reset() {
+        hitPoint = maxHitPoint;
+    }
+}
